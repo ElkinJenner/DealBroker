21502897bdb355a5ffe330dbfd97707c42866102
ElkinJenner <elkinjenner@gmail.com>
2023-05-22 15:11:54 -0500

    Modificado

--- legal.php
+++ legal.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Deal Broker - Legalidad</title>
+    <title>Deal Broker - Legal</title>
     <?php include "inc/link.php"?>
 </head>
 <body>
@@ -15,24 +15,34 @@
         <section>
             <div class="container container_website">
                 <h1 class="h1 color_s t_c">LEGALIDAD</h1><br>
-                <h4 class="f_weight color_w">El presente articulo muestra la política de Privacidad establecida por los términos en que usa
-                y protege la información que es proporcionada por
-                sus usuarios al momento de utilizar a la plataforma. Esta compañía está comprometida con la seguridad de los datos de sus
-                usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo
-                hacemos asegurando que sólo se empleará de acuerdo con los términos de este documento. Sin embargo esta Política de
-                Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente
-                esta página para asegurarse que está de acuerdo con dichos cambios.</h4><br>
-                <h2 class="f_weight color_s">Información que es recogida</h2><br>
-                <h4 class="f_weight color_w">Nuestro sitio web podrá recoger información personal por ejemplo: Nombre, información de contacto como su dirección de
-                correo electrónica e información demográfica. Así mismo cuando sea necesario podrá ser requerida información específica
-                para procesar algún pedido o realizar una entrega o facturación.
-                </h4><br>
-                <h2 class="f_weight color_s">Enlaces a Terceros</h2><br>
-                <h4 class="f_weight color_w">Este sitio web pudiera contener enlaces a otros sitios que pudieran ser de su interés. Una vez que usted de clic en
-                estos enlaces y abandone nuestra página, ya no tenemos control sobre al sitio al que es redirigido y por lo tanto no
-                somos responsables de los términos o privacidad ni de la protección de sus datos en esos otros sitios terceros. Dichos
-                sitios están sujetos a sus propias políticas de privacidad por lo cual es recomendable que los consulte para confirmar
-                que usted está de acuerdo con estas.</h4>
+                <h3 class="color_w">Esta política está regulada por la legislación peruana y en particular por: </h3><br>
+
+                <h4 class="font_t color_c">. Ley N° 29733 – Ley de Protección de Datos Personales (LPDP).</h4><br>
+                <h4 class="font_t color_c">. Decreto Supremo N° 003-2013-JUS que aprobó el Reglamento de la Ley N° 29733 (RLPDP). </h4><br>
+                <h4 class="font_t color_c">. Resolución Directoral N° 019-2013-JUS/DGPDP, que aprobó la Directiva de Seguridad de la Información (la Directiva).</h4><br>
+
+                <h4 class="font_t color_w">
+                De acuerdo con la LPDP y su RLPDP, se entiende por datos personales toda información numérica,
+                alfabética, gráfica, fotográfica, acústica, sobre hábitos personales, o de cualquier otro tipo concerniente
+                a una persona natural que la identifica o la hace identificable a través de medios que pueden ser razonablemente utilizados.<br><br>
+                Asimismo, se entiende por tratamiento de datos personales a cualquier operación o procedimiento técnico, automatizado
+                o no, que permite la recopilación, registro, organización, almacenamiento, conservación, elaboración, modificación, extracción,
+                consulta, utilización, bloqueo, supresión, comunicación por transferencia o por difusión o cualquier otra forma de procesamiento
+                que facilite el acceso, correlación o interconexión de los datos personales. Desarrolla su política de tratamiento de datos
+                personales en atención a los principios rectores establecidos en la Ley Nº 29733 – Ley de Protección de Datos Personales y por lo 
+                tanto:
+                </h4>
+                <ol class="lista_enumerada" type="1">
+                    <li>De acuerdo al principio de legalidad, rechaza la recopilación de los datos personales de nuestros Usuarios por medios
+                    fraudulentos, desleales o ilícitos.  </li>
+                    <li>Conforme al principio de consentimiento, en el tratamiento de los datos personales de nuestros Usuarios mediará su consentimiento.</li>
+                    <li>Los datos personales de nuestros Usuarios se recopilarán para una finalidad determinada, explícita y lícita, y no se extenderá a
+                        otra finalidad que no haya sido la establecida de manera inequívoca como tal al momento de su recopilación, excluyendo los casos de
+                        actividades de valor histórico, estadístico o científico
+                    cuando se utilice un procedimiento de disociación o anonimización.</li>
+                    <li>Todo tratamiento de datos personales de nuestros Usuarios será adecuado, relevante y no excesivo a la finalidad para la que estos
+                        hubiesen sido recopilados.</li>
+                </ol>
             </div>
         </section>
     </main>
