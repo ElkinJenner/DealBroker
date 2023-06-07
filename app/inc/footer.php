79fcab776f9eeef965c4f81e137809bcb69ce314
ElkinJenner <elkinjenner@gmail.com>
2023-06-07 10:47:04 -0500

    Modificado

--- app/inc/footer.php
+++ app/inc/footer.php
@@ -3,3 +3,4 @@
 <script src="../js/bar.js"></script>
 <script src="../js/utils.js"></script>
 <script src="../js/linechart.js"></script>
+<script src="../js/game.js"></script>
--- app/inc/link.php
+++ app/inc/link.php
@@ -1,6 +1,8 @@
     <link rel="stylesheet" href="../css/framework.css">
     <link rel="stylesheet" href="../css/main.css">
-     <link rel="stylesheet" href="../css/animacion.css">
+    <link rel="stylesheet" href="../css/animacion.css">
+    <link rel="stylesheet" href="../css/icomoon.css">
+    <link rel="stylesheet" href="../css/icons.css">
     <link rel="stylesheet" href="../css/responsive.css">
     <link rel="stylesheet" href="../css/game.css">
     <link rel="shortcut icon" href="../favicon.png">
