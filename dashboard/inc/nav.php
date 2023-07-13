d2ffe3f631eab51e14f5dca4a7de18871f9012f2
ElkinJenner <elkinjenner@gmail.com>
2023-07-13 11:09:14 -0500

    Modificado

--- dashboard/inc/nav.php
+++ dashboard/inc/nav.php
@@ -2,14 +2,15 @@
 <nav class="nav_dashboard" id="nav_dashboard">
     <ul>
         <code class="color_w">MENU</code>
-        <li><a href="./"><i class="bx bx-home"></i><span>Dashboard</span></a></li>
+        <li><a href="./"><i class="bx bx-home"></i><span>Home</span></a></li>
         <li><a href="staff.php"><i class="bx bx-rocket"></i><span>Staff</span></a></li>
-        <li><a href="players.php"><i class="bx bx-joystick"></i><span>Players</span></a></li>
         <li><a href="tienda.php"><i class="bx bx-basket"></i><span>Tienda</span></a></li>
         <li><a href="mensajes.php"><i class="bx bx-message-dots"></i><span>Chat</span></a></li>
-        <br><code class="color_w">GAME</code><br>
+
+        <br><code class="color_w">GAME</code>
+
+        <li><a href="players.php"><i class="bx bx-joystick"></i><span>Players</span></a></li>
         <li><a href="./"><i class="bx bx-tone"></i><span>Retos</span></a></li>
-        <li><a href="./"><i class="bx bx-navigation"></i><span>Preguntas</span></a></li>
     </ul>
 </nav>
 <ol class="dropdow_sidebar" id="btn_dropdow_sidebar">
