bf32b654ab3d3be51a959e08dce07cd7ac975b41
ElkinJenner <elkinjenner@gmail.com>
2023-07-13 15:25:57 -0500

    Modificado

--- dashboard/players.php
+++ dashboard/players.php
@@ -27,10 +27,37 @@ require_once "../action/conexion.php";
 
                     <aside class="column color_def">
                         <!--Player Item -->
-                        <div class="column-3">
-                            
+                        <?php
+                        $con = Db::connect();
+                        $Players = "SELECT * FROM Players p INNER JOIN Avatar a ON p.FotoPlayer=a.FotoAvatar ORDER BY Nombres ASC";
+                        $Resultado_Players = mysqli_query($con, $Players);
+
+                        while($r_player=mysqli_fetch_assoc($Resultado_Players)){?>
+
+                        <div class="column-2">
+                            <article class="players_item bg_def">
+                                <date_add class="activo_cuenta">
+                                    <?php if($r_player['TipoGenero']!='Otro'){?>
+                                        <div class="m_1 bg_verify btn_estado color_w">Activo</div>
+                                    <?php } else{?>
+                                        <div class="m_1 bg_danger btn_estado color_w">Inactivo</div>
+                                    <?php } ?>
+
+
+                                </date_add>
+                                <figure>
+                                <img src="<?php echo $r_player['FotoPlayer'];?>">
+                                </figure>
+                                <h5 class="f_weight color_w"><?php echo utf8_encode($r_player['Nombres']);?></h5>
+                                <small class="f_weight color_w">@<?php echo $r_player['Usuario'];?></small>
+                                <p class="redes_s">
+                                    <a href=""><i class="color_w bx bx-show-alt"></i></a>
+                                    <a href=""><i class="color_w bx bx-x"></i></a>
+                                </p>
+                            </article>
                         </div>
                         <!--Fin Player Item -->
+                        <?php } mysqli_free_result($Resultado_Players);?>
                     </aside>
                 </div>
             </main>
